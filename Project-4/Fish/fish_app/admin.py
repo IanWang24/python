5ff035fa349dbd592bdb7ff24d9c3d00f40389f0
IanWang24 <50164176+IanWang24@users.noreply.github.com>
2021-12-22 11:02:46 +0800

    Add files via upload

---
+++ Project-4/Fish/fish_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Video,Order,Profile,Customer
+# Register your models here.
+
+admin.site.register(Video)
+admin.site.register(Order)
+admin.site.register(Profile)
+admin.site.register(Customer)
